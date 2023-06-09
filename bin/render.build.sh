0e83b1fe39d1034fee459961ecc6e5b4bf8d4b7d
david00154 <davidbriggs00154@gmail.com>
2023-06-09 14:19:09 +0000

    added bin scripts

---
+++ bin/render.build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# exit on error
+set -o errexit
+
+npm i
+node ace build --ignore-ts-errors --production --no-assets
+cd build
+cp -r ../prisma .
+echo "RUNNING CI..."
+npm ci --production
+echo "RUNNING MIGRATION"
+node ace migration:run
+echo "GOING OUT OF BUILD"
+cd ..
